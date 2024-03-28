d671b10a549c93dbac786d3f039fbaf62970a6f4
PrajjwalLyzr <prajjwal@lyzr.ai>
2024-03-28 18:52:04 +0530

    created social media advisor

---
+++ app.py
@@ -0,0 +1,103 @@
+import os
+from PIL import Image
+from utils import utils
+from pathlib import Path
+import streamlit as st
+from dotenv import load_dotenv; load_dotenv()
+from lyzr import VoiceBot, ChatBot
+
+# Setup your config
+st.set_page_config(
+    page_title="Social Media Advisor",
+    layout="centered",   
+    initial_sidebar_state="auto",
+    page_icon="./logo/lyzr-logo-cut.png"
+)
+
+# Load and display the logo
+image = Image.open("./logo/lyzr-logo.png")
+st.image(image, width=150)
+
+# App title and introduction
+st.title("Social Media Advisor by Lyzr")
+st.markdown("### Welcome to the Social Media Advisor!")
+st.markdown("Social Media Advisor by Lyzr will turns your articles into ready-to-post content for LinkedIn, Instagram, Twitter, and Facebook!!!")
+
+# Custom function to style the app
+def style_app():
+    # You can put your CSS styles here
+    st.markdown("""
+    <style>
+    .app-header { visibility: hidden; }
+    .css-18e3th9 { padding-top: 0; padding-bottom: 0; }
+    .css-1d391kg { padding-top: 1rem; padding-right: 1rem; padding-bottom: 1rem; padding-left: 1rem; }
+    </style>
+    """, unsafe_allow_html=True)
+
+# Social Media Advisor Application
+data = 'data'
+audio_directory = 'audio'
+os.makedirs(data, exist_ok=True)
+os.makedirs(audio_directory, exist_ok=True)
+original_directory = os.getcwd()
+
+# replace this with your openai api key or create an environment variable for storing the key.
+API_KEY = os.getenv('OPENAI_API_KEY')
+
+ 
+def qabot_agent():
+    path = utils.get_files_in_directory(data)
+    qa_agent = ChatBot.docx_chat(
+        input_files=path
+    )
+
+    return qa_agent
+
+
+def voicebot_agent(add_summary):
+    vb = VoiceBot(api_key=API_KEY)
+    try:
+        os.chdir(audio_directory)
+        vb.text_to_speech(add_summary)
+    finally:
+        os.chdir(original_directory)
+
+
+
+if __name__ == "__main__":
+    style_app() 
+    article = st.file_uploader('Upload your article documet',type=["docx"])
+    if article:
+        utils.save_uploaded_file(directory=data, uploaded_file=article)
+        file = utils.file_checker()
+        if len(file)>0:
+            if st.button('Create'):
+                agent = qabot_agent()
+                response = utils.social_media(agent=agent)
+                results = utils.prompts(agent=agent)
+                if response:
+                    utils.get_response(response=response)
+                    if results:
+                        voicebot_agent(add_summary=results['Voice_Ad'])
+                        st.subheader('Voice Advertisment')
+                        files = utils.get_files_in_directory(audio_directory)
+                        audio_file = files[0]
+                        st.audio(audio_file)  
+                        st.markdown('---')
+                        st.subheader('Instagram')
+                        insta_image = utils.instagram_img(results['Instagram_post'])
+                        st.image(insta_image, caption='Instagram Image', use_column_width=True)
+
+    else:
+        utils.remove_existing_files(data)
+        st.warning('Please Upload article document file')
+    
+    with st.expander("ℹ️ - About this App"):
+        st.markdown("""
+        This app uses Lyzr Agent's to create the social media manager, it uses's ChatBot for RAG, and VoiceBot for audio file, and OpenAI dall-e-3 for image generation. For any inquiries or issues, please contact Lyzr.
+        
+        """)
+        st.link_button("Lyzr", url='https://www.lyzr.ai/', use_container_width = True)
+        st.link_button("Book a Demo", url='https://www.lyzr.ai/book-demo/', use_container_width = True)
+        st.link_button("Discord", url='https://discord.gg/nm7zSyEFA2', use_container_width = True)
+        st.link_button("Slack", url='https://join.slack.com/t/genaiforenterprise/shared_invite/zt-2a7fr38f7-_QDOY1W1WSlSiYNAEncLGw', use_container_width = True)
---
+++ utils/utils.py
@@ -0,0 +1,108 @@
+import os
+import shutil
+import streamlit as st
+from dotenv import load_dotenv; load_dotenv()
+from openai import OpenAI
+
+
+API_KEY = os.getenv('OPENAI_API_KEY')
+
+def remove_existing_files(directory):
+    for filename in os.listdir(directory):
+        file_path = os.path.join(directory, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            st.error(f"Error while removing existing files: {e}")
+
+def get_files_in_directory(directory):
+    # This function help us to get the file path along with filename.
+    files_list = []
+
+    if os.path.exists(directory) and os.path.isdir(directory):
+        for filename in os.listdir(directory):
+            file_path = os.path.join(directory, filename)
+            
+            if os.path.isfile(file_path):
+                files_list.append(file_path)
+
+    return files_list
+
+def save_uploaded_file(directory, uploaded_file):
+    remove_existing_files(directory=directory)
+    file_path = os.path.join(directory, uploaded_file.name)
+    with open(file_path, "wb") as file:
+        file.write(uploaded_file.read())
+    st.success("File uploaded successfully")
+
+def file_checker():
+    file = []
+    for filename in os.listdir('data'):
+        file_path = os.path.join('data', filename)
+        file.append(file_path)
+
+    return file
+
+
+def social_media(agent):
+    content = {}
+    prompts = {'LinkedIn': """
+                        Craft an engaging LinkedIn post based on this article. 
+                        Share a concise yet compelling overview of article's key points to capture the audience's attention. 
+                        Remember to include relevant hashtags and a call-to-action to encourage further engagement and drive traffic to your article. Let's create an impactful post that resonates with your LinkedIn network!
+            """,
+
+            "Twitter":"""
+                        Craft a captivating tweet inspired by the essence of this article. 
+                        Condense the key insights into 1-2 punchy lines that grab attention and leave readers intrigued. 
+                        Ensure the tweet is concise yet impactful, and don't forget to add relevant hashtags to expand your reach. Let's compose a tweet that sparks curiosity and drives engagement!
+
+            """,
+
+
+            "Facebook":"""
+                        Compose an engaging Facebook post that encapsulates the essence of this article. 
+                        Share a brief summary that highlights the most compelling aspects, aiming to captivate your audience's interest. 
+                        Consider adding an eye-catching image or video to enhance the post's appeal, and encourage interaction by asking a thought-provoking question or inviting comments. Let's craft a post that resonates with your Facebook followers and sparks meaningful conversations!
+
+            """}
+
+    for platfrom, prompt in prompts.items():
+        response = agent.chat(prompt)
+        content[platfrom] = response.response
+
+    return content  
+
+def prompts(agent):
+    results = {}
+    prompt = {'Voice_Ad':'create a article summary for advertisment in 150-200 words',
+               'Instagram_post':'create a article summary for instagram post in 50-70 words' }  
+
+    for platfrom, prompt in prompt.items():        
+        response = agent.chat(prompt)
+        results[platfrom] = response.response
+
+    return results
+
+
+def get_response(response:dict):
+    for platfrom, response in response.items():
+        st.subheader(platfrom)
+        st.write(response)
+        st.markdown("---")  
+
+def instagram_img(prompt):
+    client = OpenAI(api_key=API_KEY)
+    response = client.images.generate(
+    model="dall-e-3",
+    prompt=prompt,
+    size="1024x1024",
+    quality="standard",
+    n=1,
+    )
+
+    image_url = response.data[0].url
+    return image_url
